ad1d726db4c75c2b175965173eca6f0c4bf98c61
Claudia Dumitriu <claudiadumitriu@Claudias-Laptop.local>
2025-04-07 04:45:57 +0300

    added landing-page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -4,18 +4,13 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
 })
 export class LandingPageComponent {
   constructor(private router: Router) {}
 
-  registerPage() {
-    this.router.navigate(['/register']);
-  }
-
-  loginPage() {
-    this.router.navigate(['/login']);
+  navigateTo(page: string) {
+    this.router.navigate([`/${page}`]);
   }
 }
